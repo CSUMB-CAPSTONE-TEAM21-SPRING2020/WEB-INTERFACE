366cceb4be13808a587928dbc574b7d073cad8b0
bwernick <bwernick@csumb.edu>
2020-02-18 15:25:31 -0800

    login page & bootstrap styling

---
+++ src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <p>Home page under construction</p>
+            </div>
+        );
+    }
+}
